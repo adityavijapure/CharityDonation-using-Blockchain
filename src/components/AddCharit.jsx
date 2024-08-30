2963bf153c1624a64a4092d5ed9bd13709ffa2e8
Aditya Vijapure <109146516+adityavijapure@users.noreply.github.com>
2024-08-31 02:50:50 +0530

    update dash bourd

---
+++ src/components/AddCharit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const addCharit = () => {
+  return (
+    <div>
+      dzsdz
+    </div>
+  )
+}
+
+export default addCharit;
--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -2,46 +2,59 @@ import React from "react";
 import logo from "../assets/logo.png";
 import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { MdDashboard } from "react-icons/md";
+import { IoMdPersonAdd } from "react-icons/io";
+import { FaPen } from "react-icons/fa";
+import { RiAdminFill } from "react-icons/ri";
+import { GrUserAdmin } from "react-icons/gr";
+import '../styles/style.css'
+import { BiLeftArrowAlt } from "react-icons/bi";
 const NavButton = () => {
   return (
-    <div className="w-1/6 h-screen bg-gray-700 text-white">
-      <div className="w-full h-auto flex justify-between px-3 mb-5  items-center">
-        <img src={logo} alt="logo" className="w-[70px]  p-2" />
-        <h2>CHAIN DONATION</h2>
-      </div>
-      <div className="mt-5">
-        <div className="flex relative align-middle items-center gap-6 text-[20px] w-full justify-center hover:border-l-4  hover:bg-slate-800">
-          <i>
-            <FaHome />
-          </i>
-          <a>Home</a>
-        </div>
-      </div>
-      <div className="mt-5">
-        <div className="flex relative align-middle items-center gap-6 text-[20px] w-full justify-center hover:border-l-4  hover:bg-slate-800">
-          <i>
-            <FaHome />
-          </i>
-          <a>EDIT PROFILE</a>
-        </div>
-      </div>
-      <div className="mt-5">
-        <div className="flex relative align-middle items-center gap-6 text-[20px] w-full justify-center hover:border-l-4  hover:bg-slate-800">
-          <i>
-            <FaHome />
-          </i>
-          <a>ADD  CHARITY</a>
-        </div>
-      </div>
-      <div className="mt-5">
-        <div className="flex relative align-middle items-center gap-6 text-[20px] w-full justify-center hover:border-l-4  hover:bg-slate-800">
-          <i>
-            <FaHome />
-          </i>
-          <a>Home</a>
-        </div>
-      </div>
+    <div className='leftbox'>
+    <div className='logo'>
+        <img src={logo} className="w-5/12" />
     </div>
+      <div className='menuBox'>
+       <div > 
+        <ul className='menuList' >
+       <Link to ='/Dashboard' className='Link'>
+            <li>
+               <i >
+                <MdDashboard />
+               </i>
+               <a >Dashboard</a>
+            </li>
+        </Link>
+        <Link to ='/updateProfile' className='Link'>
+            <li>
+               <i>
+                <FaPen />
+               </i>
+                <a href='#' >Update Profile</a>
+            </li>
+        </Link>
+        <Link to ='/addCharity' className='Link'>
+            <li>
+               <i>
+                <IoMdPersonAdd />
+               </i>
+               <a href='#' >Add Charity</a>
+            </li>
+            </Link>
+            <Link to ='/' className='Link'> 
+            <li>
+               <i>
+                <BiLeftArrowAlt />
+               </i>
+               <a href='#' >Back To website</a>
+            </li>
+            </Link>
+            
+        </ul>
+       </div>
+    </div>
+</div>
   );
 };
 
